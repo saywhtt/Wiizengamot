a365231726eb70224ea5e44c9eefe5486f914e31
Sergey Simonov <welcometoriverwood@gmail.com>
2021-01-17 12:05:41 +0300

    Presenters: separated getAll and search logic

--- app/src/main/java/edu/born/flicility/presenters/PhotoListPresenter.kt
+++ app/src/main/java/edu/born/flicility/presenters/PhotoListPresenter.kt
@@ -2,6 +2,4 @@ package edu.born.flicility.presenters
 
 interface PhotoListPresenter {
     fun getPhotos()
-    fun getPhotosByNewQuery(query: String)
-    fun getNextPageByCurrentQuery()
 }
--- app/src/main/java/edu/born/flicility/presenters/PhotoListPresenterImpl.kt
+++ app/src/main/java/edu/born/flicility/presenters/PhotoListPresenterImpl.kt
@@ -1,90 +1,26 @@
 package edu.born.flicility.presenters
 
 import edu.born.flicility.model.Photo
-import edu.born.flicility.network.PhotoSearchResponse
 import edu.born.flicility.network.PhotoService
-import edu.born.flicility.presenters.PhotoListPresenterImpl.Query.*
 import edu.born.flicility.views.PhotoListView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+private val TAG = PhotoListPresenterImpl::class.java.simpleName
+
 class PhotoListPresenterImpl(private val service: PhotoService) : BasePresenter<PhotoListView>(), PhotoListPresenter {
-
-    private val TAG = PhotoListPresenterImpl::class.java.canonicalName
-
-    sealed class Query(var currentPage: Int = 0,
-                       var isNoMoreResults: Boolean = false) {
-        class All(currentPage: Int = 0,
-                  isNoMoreResults: Boolean = false) : Query(currentPage, isNoMoreResults)
-
-        class Search(var query: String?,
-                     currentPage: Int = 0,
-                     isNoMoreResults: Boolean = false) : Query(currentPage, isNoMoreResults)
-    }
-
-    private val all = All()
-    private val search = Search(query = null)
-
-    var currentQuery: Query = all
-
-    override fun getNextPageByCurrentQuery() {
-        when (currentQuery) {
-            is All -> getPhotos()
-            is Search -> {
-                getPhotosByQuery((currentQuery as Search).query)
-            }
-        }
-    }
-
-    override fun getPhotosByNewQuery(query: String) {
-        search.query = query
-        search.currentPage = 0
-        search.isNoMoreResults = false
-
-        currentQuery = search
-        getPhotosByQuery(search.query)
-    }
-
-    private fun getPhotosByQuery(query: String?) {
-        if (currentQuery.isNoMoreResults) return
-        query?.let {
-            view?.startDownloading()
-            service.getPhotosByQuery(page = ++currentQuery.currentPage, query = query)
-                    .enqueue(object : Callback<PhotoSearchResponse> {
-
-                        override fun onResponse(call: Call<PhotoSearchResponse>, response: Response<PhotoSearchResponse>) {
-                            response.body()?.let {
-                                if (it.results.isEmpty()) currentQuery.isNoMoreResults = true
-                                else view?.updateData(it.results)
-                                view?.endDownloading()
-                            }
-                        }
-
-                        override fun onFailure(call: Call<PhotoSearchResponse>, t: Throwable) = t.printStackTrace()
-                    })
-        }
-    }
+    private var query = Query.All()
 
     override fun getPhotos() {
-        when (currentQuery) {
-            is Search -> {
-                search.query = null
-                search.currentPage = 0
-                currentQuery = all
-            }
-            is All -> {
-                if (all.isNoMoreResults) return
-            }
-        }
+        if (query.isNoMoreResults) return
         view?.startDownloading()
-        service.getPhotos(page = ++all.currentPage)
+        service.getPhotos(page = ++query.currentPage)
                 .enqueue(object : Callback<List<Photo>> {
-
                     override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
                         response.body()?.let {
-                            if (it.isEmpty()) currentQuery.isNoMoreResults = true
-                            else view?.updateData(it)
+                            if (it.isEmpty()) query.isNoMoreResults = true
+                            else view?.update(it)
                             view?.endDownloading()
                         }
                     }
@@ -92,5 +28,4 @@ class PhotoListPresenterImpl(private val service: PhotoService) : BasePresenter<
                     override fun onFailure(call: Call<List<Photo>>, t: Throwable) = t.printStackTrace()
                 })
     }
-
 }

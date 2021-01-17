cb07d7d101d249c8f48dd8e1061fa74bcc05e660
Sergey Simonov <welcometoriverwood@gmail.com>
2021-01-17 12:09:09 +0300

    VisibleFragment: added initialization app and hideKeyboard()

--- app/src/main/java/edu/born/flicility/fragments/VisibleFragment.kt
+++ app/src/main/java/edu/born/flicility/fragments/VisibleFragment.kt
@@ -5,15 +5,26 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
+import edu.born.flicility.app.App
 import edu.born.flicility.service.PERMISSION_PRIVATE
 import edu.born.flicility.service.SHOW_NOTIFICATION
 
 open class VisibleFragment : Fragment() {
+    protected lateinit var app: App
+
     private val offNotificationReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             resultCode = Activity.RESULT_CANCELED
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        app = activity?.applicationContext as App
     }
 
     override fun onResume() {
@@ -29,5 +40,13 @@ open class VisibleFragment : Fragment() {
     override fun onPause() {
         super.onPause()
         activity?.unregisterReceiver(offNotificationReceiver)
+    }
+
+    protected open fun hideKeyboard() {
+        activity?.let {
+            val imm = it.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            val view = it.currentFocus ?: View(it)
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }

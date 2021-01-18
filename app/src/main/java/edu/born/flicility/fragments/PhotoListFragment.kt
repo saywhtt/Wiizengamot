45852b416bf7769e7a0a41cee8492051e6b0412a
Sergey Simonov <welcometoriverwood@gmail.com>
2021-01-18 19:37:47 +0300

    renamed fragment_photo_gallery.xml -> fragment_photo_list.xml

--- app/src/main/java/edu/born/flicility/fragments/PhotoListFragment.kt
+++ app/src/main/java/edu/born/flicility/fragments/PhotoListFragment.kt
@@ -41,7 +41,7 @@ class PhotoListFragment : VisibleFragment(), PhotoListView {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        val view = inflater.inflate(R.layout.fragment_photo_list, container, false)
         recyclerView = view.findViewById(R.id.recycler_view)
         recyclerView.adapter = getPreparedAdapter()
         recyclerView.layoutManager = GridLayoutManager(activity, 3)
--- app/src/main/java/edu/born/flicility/fragments/PhotoSearchFragment.kt
+++ app/src/main/java/edu/born/flicility/fragments/PhotoSearchFragment.kt
@@ -38,7 +38,7 @@ class PhotoSearchFragment : VisibleFragment(), PhotoSearchView {
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        val view = inflater.inflate(R.layout.fragment_photo_list, container, false)
         recyclerView = view.findViewById(R.id.recycler_view)
         recyclerView.adapter = getPreparedAdapter()
         recyclerView.layoutManager = GridLayoutManager(activity, 3)

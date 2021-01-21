69038e8ef1faf846898a1ec0d9ad760f5e2418fa
Sergey Simonov <welcometoriverwood@gmail.com>
2021-01-21 20:57:19 +0300

    added PhotoPagerActivity

--- app/src/main/java/edu/born/flicility/activities/PhotoPagerActivity.kt
+++ app/src/main/java/edu/born/flicility/activities/PhotoPagerActivity.kt
@@ -2,14 +2,35 @@ package edu.born.flicility.activities
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import edu.born.flicility.R
 import edu.born.flicility.fragments.PhotoFragment
+import edu.born.flicility.model.Photo
 
-class PhotoPagerActivity: AbstractFragmentActivity() {
+class PhotoPagerActivity : AppCompatActivity() {
     companion object {
-        fun newIntent(context: Context?): Intent {
-            return Intent(context, PhotoPagerActivity::class.java)
-        }
+        private val EXTRA_PHOTO_LIST = "PHOTO_LIST"
+        fun newIntent(context: Context?, photoList: ArrayList<Photo>) =
+                Intent(context, PhotoPagerActivity::class.java).putParcelableArrayListExtra(EXTRA_PHOTO_LIST, photoList)
     }
 
-    override fun getFragment() = PhotoFragment()
+    private lateinit var viewPager: ViewPager
+    private lateinit var photos: List<Photo>
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_photo_pager)
+
+        photos = intent.getParcelableArrayListExtra<Photo>(EXTRA_PHOTO_LIST) as List<Photo>
+
+        viewPager = findViewById(R.id.photo_view_pager)
+        viewPager.adapter = object : FragmentStatePagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            override fun getItem(position: Int) = PhotoFragment.newInstance(photos[position])
+            override fun getCount() = photos.size
+        }
+    }
 }

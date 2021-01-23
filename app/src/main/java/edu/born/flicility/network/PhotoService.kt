9b9fc45c80a5d7bab6f95483b0ebc03970cbb320
Sergey Simonov <welcometoriverwood@gmail.com>
2021-01-23 19:05:54 +0300

    PhotoService: added getImage, download photo by bytes

--- app/src/main/java/edu/born/flicility/network/PhotoService.kt
+++ app/src/main/java/edu/born/flicility/network/PhotoService.kt
@@ -3,10 +3,13 @@ package edu.born.flicility.network
 import edu.born.flicility.model.Photo
 import edu.born.flicility.network.API.AUTHORIZATION_HEADER
 import edu.born.flicility.network.API.PER_PAGE
+import io.reactivex.rxjava3.core.Observable
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface PhotoService {
     @GET("photos")
@@ -19,4 +22,7 @@ interface PhotoService {
                          @Query("page") page: Int,
                          @Query("per_page") per_page: Int = PER_PAGE,
                          @Header("Authorization") auth: String = AUTHORIZATION_HEADER): Call<PhotoSearchResponse>
+
+    @GET
+    fun getImage(@Url url: String): Observable<ResponseBody>
 }

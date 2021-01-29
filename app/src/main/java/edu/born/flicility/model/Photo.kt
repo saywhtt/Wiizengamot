7ddba96ded6445655e37f16cffda4d2b80f1dac3
Sergey Simonov <welcometoriverwood@gmail.com>
2021-01-29 19:07:48 +0300

    photo.title -> photo.description (nullable)

--- app/src/main/java/edu/born/flicility/model/Photo.kt
+++ app/src/main/java/edu/born/flicility/model/Photo.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Photo(@SerializedName("title") var title: String,
-                 @SerializedName("id") var id: String,
+data class Photo(@SerializedName("id") var id: String,
+                 @SerializedName("description") var description: String?,
                  @SerializedName("urls") var urls: Urls) : Parcelable

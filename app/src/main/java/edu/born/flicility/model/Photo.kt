17155c5d5d06069398cfae4e3db95b331e303cb1
Sergey Simonov <welcometoriverwood@gmail.com>
2021-02-20 16:40:13 +0300

    Photo: optimize imports

--- app/src/main/java/edu/born/flicility/model/Photo.kt
+++ app/src/main/java/edu/born/flicility/model/Photo.kt
@@ -3,7 +3,6 @@ package edu.born.flicility.model
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
-//import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Photo(@SerializedName("id") var id: String,

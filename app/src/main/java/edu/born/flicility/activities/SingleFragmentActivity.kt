975c0b912d24c56f32cced3e9515e74341b4ec92
Sergey Simonov <welcometoriverwood@gmail.com>
2021-02-27 12:05:35 +0300

    used extension funcs for SingleFragmentActivity (management fragments)

--- app/src/main/java/edu/born/flicility/activities/SingleFragmentActivity.kt
+++ app/src/main/java/edu/born/flicility/activities/SingleFragmentActivity.kt
@@ -2,27 +2,18 @@ package edu.born.flicility.activities
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.*
 import edu.born.flicility.R
 import edu.born.flicility.fragments.PhotoListFragment
 import edu.born.flicility.fragments.PhotoPagerFragment
 import edu.born.flicility.fragments.PhotoSearchFragment
-import edu.born.flicility.fragments.PhotoWebPageFragment
+import edu.born.flicility.utils.*
 
 class SingleFragmentActivity : AppCompatActivity() {
 
     companion object {
-        // keys
-        const val START_SEARCH_REQUEST_KEY = "START_SEARCH_REQUEST_KEY"
-        const val START_PHOTO_PAGER_REQUEST_KEY = "START_PHOTO_PAGER_REQUEST_KEY"
-        const val CLOSE_FRAGMENT_REQUEST_KEY = "END_PHOTO_PAGER_REQUEST_KEY"
-        const val END_PHOTO_PAGER_BY_ALL_REQUEST_KEY = "END_PHOTO_PAGER_BY_ALL_REQUEST_KEY"
-        const val END_PHOTO_PAGER_BY_SEARCH_REQUEST_KEY = "END_PHOTO_PAGER_BY_SEARCH_REQUEST_KEY"
-
         //bundles
         const val PHOTOS_ARG = "PHOTOS_ARG"
         const val PHOTO_POSITION_ARG = "PHOTO_POSITION_ARG"
@@ -42,49 +33,24 @@ class SingleFragmentActivity : AppCompatActivity() {
     }
 
     private fun prepareListeners() {
-        setFragmentResultListenerByKey(START_SEARCH_REQUEST_KEY,
-                FragmentResultListener { _, _ -> replaceFragment<PhotoSearchFragment>() })
-        setFragmentResultListenerByKey(START_PHOTO_PAGER_REQUEST_KEY,
-                FragmentResultListener { _, result ->
-                    val (photos, position, query) = PhotoPagerFragment.splitArguments(result)
-                    val fragment = PhotoPagerFragment.newInstance(position, photos, query)
-                    replaceFragment(fragment)
-                })
-        setFragmentResultListenerByKey(CLOSE_FRAGMENT_REQUEST_KEY,
-                FragmentResultListener { _, _ -> supportFragmentManager.popBackStack() })
+        setFragmentResultListener(START_SEARCH_REQUEST_KEY) { _, _ ->
+            replaceFragment<PhotoSearchFragment>(getLayoutResId())
+        }
+        setFragmentResultListener(START_PHOTO_PAGER_REQUEST_KEY) { _, result ->
+            val (photos, position, query) = PhotoPagerFragment.splitArguments(result)
+            val fragment = PhotoPagerFragment.newInstance(position, photos, query)
+            replaceFragment(fragment, getLayoutResId())
+        }
+        setFragmentResultListener(CLOSE_FRAGMENT_REQUEST_KEY) { _, _ ->
+            supportFragmentManager.popBackStack()
+        }
     }
-
-    private fun setFragmentResultListenerByKey(key: String, listener: FragmentResultListener) =
-            supportFragmentManager.setFragmentResultListener(key, this, listener)
 
     private fun startMainFragment() =
             with(supportFragmentManager) {
                 findFragmentByTag(PhotoListFragment::class.java.name)
-                        ?: addFragment<PhotoListFragment>()
+                        ?: addFragment<PhotoListFragment>(getLayoutResId(), false)
             }
-
-    private inline fun <reified T : Fragment> replaceFragment() {
-        supportFragmentManager.commit {
-            replace<T>(R.id.fragment_container)
-            setReorderingAllowed(true)
-            addToBackStack(T::class.java.name)
-        }
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.commit {
-            replace(R.id.fragment_container, fragment)
-            setReorderingAllowed(true)
-            addToBackStack(fragment::class.java.name)
-        }
-    }
-
-    private inline fun <reified T : Fragment> addFragment() {
-        supportFragmentManager.commit {
-            add<T>(R.id.fragment_container)
-            setReorderingAllowed(true)
-        }
-    }
 
     @LayoutRes
     private fun getLayoutResId(): Int = R.layout.activity_fragment
---
+++ app/src/main/java/edu/born/flicility/utils/Fragments.kt
@@ -0,0 +1,45 @@
+package edu.born.flicility.utils
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.*
+
+const val START_SEARCH_REQUEST_KEY = "START_SEARCH_REQUEST_KEY"
+const val START_PHOTO_PAGER_REQUEST_KEY = "START_PHOTO_PAGER_REQUEST_KEY"
+const val CLOSE_FRAGMENT_REQUEST_KEY = "END_PHOTO_PAGER_REQUEST_KEY"
+const val END_PHOTO_PAGER_BY_ALL_REQUEST_KEY = "END_PHOTO_PAGER_BY_ALL_REQUEST_KEY"
+const val END_PHOTO_PAGER_BY_SEARCH_REQUEST_KEY = "END_PHOTO_PAGER_BY_SEARCH_REQUEST_KEY"
+
+fun AppCompatActivity.setFragmentResultListener(requestKey: String,
+                                                listener: ((requestKey: String, bundle: Bundle) -> Unit)) {
+    supportFragmentManager.setFragmentResultListener(requestKey,
+            this,
+            listener as FragmentResultListener)
+}
+
+inline fun <reified T : Fragment> AppCompatActivity.replaceFragment(resId: Int, addBackStack: Boolean = true) {
+    supportFragmentManager.commit {
+        replace<T>(resId)
+        setReorderingAllowed(true)
+        if (addBackStack)
+            addToBackStack(T::class.java.name)
+    }
+}
+
+inline fun <reified T : Fragment> AppCompatActivity.addFragment(resId: Int, addBackStack: Boolean = true) {
+    supportFragmentManager.commit {
+        add<T>(resId)
+        setReorderingAllowed(true)
+        if (addBackStack)
+            addToBackStack(T::class.java.name)
+    }
+}
+
+fun AppCompatActivity.replaceFragment(fragment: Fragment, resId: Int, addBackStack: Boolean = true) {
+    supportFragmentManager.commit {
+        replace(resId, fragment)
+        setReorderingAllowed(true)
+        if (addBackStack)
+            addToBackStack(fragment::class.java.name)
+    }
+}
